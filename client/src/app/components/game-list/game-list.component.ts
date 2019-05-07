8a9e61378781e5bf3836c239ebda0f06138b1076
sergio <cbi2143044162@titlani.uam.mx>
2019-05-07 18:52:34 -0500

    template para aplicaciones implementando rest API

---
+++ client/src/app/components/game-list/game-list.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit, HostBinding } from '@angular/core';
+
+import { GamesService } from '../../services/games.service';
+import { Game } from 'src/app/models/Game';
+
+
+@Component({
+  selector: 'app-game-list',
+  templateUrl: './game-list.component.html',
+  styleUrls: ['./game-list.component.css']
+})
+export class GameListComponent implements OnInit {
+
+  @HostBinding('class') classes = 'row';
+
+  games: any = [];
+
+  constructor(private gamesService: GamesService) {  }
+
+  ngOnInit() {      
+    this.getGames();
+  }
+
+  getGames() {
+    this.gamesService.getGames().subscribe(
+      res => {
+       this.games = res;
+      },
+      err => console.error(err)
+    );
+  }
+
+  deleteGame(id: string) {
+    //  console.log(id);
+    this.gamesService.deleteGame(id).subscribe(
+      res => {
+        console.log(res);
+        this.getGames();       
+      },
+      err => {
+        console.log(err)     
+      }
+    );
+     
+  }
+
+  // editGame(id: string) {
+  //   console.log(id);
+    
+  // }
+
+}
